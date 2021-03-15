a1d4e2c74fd86e8b5f96e5faacf262b1daa6c4a0
Angelo Stavrow <contact@angelostavrow.com>
2021-03-15 15:28:52 -0400

    Use idiomatic test style for utilities

--- utilities_test.go
+++ utilities_test.go
@@ -3,156 +3,103 @@ package main
 import (
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSplitAnyStringWithMultipleSeparators(t *testing.T) {
-	// Given
-	testString := "hello_there wild-world.txt"
-	separators := " -_."
-	wanted := "hellotherewildworldtxt"
+func TestSplitAny(t *testing.T) {
+	testCases := map[string]struct {
+		givenString     string
+		givenSeparators string
+		expected        string
+	}{
+		"multiple_separators": {"hello_there wild-world.txt", " -_.", "hellotherewildworldtxt"},
+	}
 
-	// When
-	result := strings.Join(SplitAny(testString, separators), "")
-
-	// Then
-	if result != "hellotherewildworldtxt" {
-		t.Fatalf(`SplitAny() = %s, want %s`, result, wanted)
+	for name, test := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result := strings.Join(SplitAny(test.givenString, test.givenSeparators), "")
+			assert.Equal(t, test.expected, result)
+		})
 	}
 }
 
-func TestConvertToHashtagOnSingleWordString(t *testing.T) {
-	// Given
-	testString := "potatoes"
-	wanted := "#potatoes"
+func TestConvertToHashtag(t *testing.T) {
+	testCases := map[string]struct {
+		given    string
+		expected string
+	}{
+		"single-word string":   {"potatoes", "#potatoes"},
+		"multiple-word string": {"baked-potatoes", "#bakedPotatoes"},
+	}
 
-	// When
-	result := ConvertToHashtag(testString)
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`ConvertToHashtag() = %s, want %s`, result, wanted)
+	for name, test := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result := ConvertToHashtag(test.given)
+			assert.Equal(t, test.expected, result)
+		})
 	}
 }
 
-func TestConvertToHashtagOnMultipleWordString(t *testing.T) {
-	// Given
-	testString := "baked-potatoes"
-	wanted := "#bakedPotatoes"
+func TestImageIsLocal(t *testing.T) {
+	testCases := map[string]struct {
+		givenTestPath string
+		givenBaseURL  string
+		expected      bool
+	}{
+		"relative path":                       {"/images/picture.jpg", "https://example.com", true},
+		"absolute path w/ same base URL":      {"https://example.com/images/picture.jpg", "https://example.com", true},
+		"absolute path w/ different base URL": {"https://example.com/images/picture.jpg", "https://write.as", false},
+	}
 
-	// When
-	result := ConvertToHashtag(testString)
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`ConvertToHashtag() = %s, want %s`, result, wanted)
+	for name, test := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result := ImageIsLocal(test.givenTestPath, test.givenBaseURL)
+			assert.Equal(t, test.expected, result)
+		})
 	}
 }
 
-func TestImageIsLocalIfRelativePath(t *testing.T) {
-	// Given
-	testPath := "/images/picture.jpg"
-	baseURL := "https://example.com"
-	wanted := true
+func TestScanConfigForBaseUrl(t *testing.T) {
+	testCases := map[string]struct {
+		givenConfigPath   string
+		givenConfigFormat string
+		expected          string
+	}{
+		"valid config file":   {"testdata/config-valid.toml", "toml", "https://example.com/"},
+		"invalid config file": {"testdata/config-noBaseURL.toml", "toml", "No baseURL value found"},
+	}
 
-	// When
-	result := ImageIsLocal(testPath, baseURL)
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`ImageIsLocal() = %t, want %t`, result, wanted)
+	for name, test := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result, err := ScanConfigForBaseUrl(test.givenConfigPath, test.givenConfigFormat)
+			if err != nil {
+				assert.EqualError(t, err, test.expected)
+			} else {
+				assert.Equal(t, test.expected, result)
+			}
+		})
 	}
 }
 
-func TestImageIsLocalIfAbsolutePathWithSameBaseURL(t *testing.T) {
-	// Given
-	testPath := "https://example.com/images/picture.jpg"
-	baseURL := "https://example.com"
-	wanted := true
-
-	// When
-	result := ImageIsLocal(testPath, baseURL)
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`ImageIsLocal() = %t, want %t`, result, wanted)
-	}
-}
-
-func TestImageIsLocalIfAbsolutePathWithDifferentBaseURL(t *testing.T) {
-	// Given
-	testPath := "https://example.com/images/picture.jpg"
-	baseURL := "https://write.as"
-	wanted := false
-
-	// When
-	result := ImageIsLocal(testPath, baseURL)
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`ImageIsLocal() = %t, want %t`, result, wanted)
-	}
-}
-
-func TestScanConfigForBaseUrlWithValidConfigFile(t *testing.T) {
-	// Given
-	configFilePath := "testdata/config-valid.toml"
-	configFileFormat := "toml"
-	wanted := "https://example.com/"
-
-	// When
-	result, err := ScanConfigForBaseUrl(configFilePath, configFileFormat)
-	if err != nil {
-		t.Fatalf(`ScanConfigForBaseUrl() threw error %s`, err)
+func TestScanConfigForLanguage(t *testing.T) {
+	testCases := map[string]struct {
+		givenConfigPath   string
+		givenConfigFormat string
+		expected          string
+	}{
+		"valid config file":   {"testdata/config-valid.toml", "toml", "en"},
+		"invalid config file": {"testdata/config-noLang.toml", "toml", "No language found"},
 	}
 
-	// Then
-	if result != wanted {
-		t.Fatalf(`ScanConfigForBaseUrl() = %s, want %s`, result, wanted)
-	}
-}
-
-func TestScanConfigForBaseUrlWithInvalidConfigFile(t *testing.T) {
-	// Given
-	configFilePath := "testdata/config-noBaseUrl.toml"
-	configFileFormat := "toml"
-
-	// When
-	result, err := ScanConfigForBaseUrl(configFilePath, configFileFormat)
-
-	// Then
-	if err == nil {
-		t.Fatalf(`ScanConfigForBaseUrl() = %s, want error`, result)
-	}
-}
-
-func TestScanConfigForLanguageWithValidConfigFile(t *testing.T) {
-	// Given
-	configFilePath := "testdata/config-valid.toml"
-	configFileFormat := "toml"
-	wanted := "en"
-
-	// When
-	result, err := ScanConfigForLanguage(configFilePath, configFileFormat)
-	if err != nil {
-		t.Fatalf(`ScanConfigForBaseUrl() threw error %s`, err)
-	}
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`ScanConfigForLanguage() = %s, want %s`, result, wanted)
-	}
-}
-
-func TestScanConfigForLanguageWithInvalidConfigFile(t *testing.T) {
-	// Given
-	configFilePath := "testdata/config-noLang.toml"
-	configFileFormat := "toml"
-
-	// When
-	result, err := ScanConfigForLanguage(configFilePath, configFileFormat)
-
-	// Then
-	if err == nil {
-		t.Fatalf(`ScanConfigForLanguage() = %s, want error`, result)
+	for name, test := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result, err := ScanConfigForLanguage(test.givenConfigPath, test.givenConfigFormat)
+			if err != nil {
+				assert.EqualError(t, err, test.expected)
+			} else {
+				assert.Equal(t, test.expected, result)
+			}
+		})
 	}
 }
