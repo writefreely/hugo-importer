0acedee2e3ba391d9a57c3c1debad8df5bf1d4c5
Angelo Stavrow <contact@angelostavrow.com>
2021-03-15 16:48:34 -0400

    Update short code tests for idiomatic test setup

--- parser_test.go
+++ parser_test.go
@@ -3,122 +3,35 @@ package main
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestScanContentForShortCodesForSimpleGist(t *testing.T) {
-	// Given
-	shortcode := "{{< gist spf13 7896402 >}}"
-	wanted := "https://gist.github.com/spf13/7896402"
-
-	// When
-	result := scanContentForShortcodes(shortcode)
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`scanContentForShortcodes() = %s, want %s`, result, wanted)
+func TestScanContentForShortCodes(t *testing.T) {
+	testCases := map[string]struct {
+		given    string
+		expected string
+	}{
+		"simple gist":           {"{{< gist spf13 7896402 >}}", "https://gist.github.com/spf13/7896402"},
+		"complex gist":          {"{{< gist spf13 7896402 \"img.html\" >}}", "https://gist.github.com/spf13/7896402"},
+		"simple instagram":      {"{{< instagram BWNjjyYFxVx >}}", "https://www.instagram.com/p/BWNjjyYFxVx"},
+		"complex instagram":     {"{{< instagram BWNjjyYFxVx hidecaption >}}", "https://www.instagram.com/p/BWNjjyYFxVx"},
+		"twitter":               {"{{< tweet 877500564405444608 >}}", "https://twitter.com/twitter/status/877500564405444608"},
+		"vimeo":                 {"{{< tweet 877500564405444608 >}}", "https://twitter.com/twitter/status/877500564405444608"},
+		"simple youtube":        {"{{< youtube w7Ft2ymGmfc >}}", "https://www.youtube.com/watch?v=w7Ft2ymGmfc"},
+		"complex youtube":       {"{{< youtube id=\"w7Ft2ymGmfc\" autoplay=\"true\" >}}", "https://www.youtube.com/watch?v=w7Ft2ymGmfc"},
+		"unsupported shortcode": {"{{< highlight html >}}", "{{< highlight html >}}"},
 	}
-}
 
-func TestScanContentForShortCodesForComplexGist(t *testing.T) {
-	// Given
-	shortcode := "{{< gist spf13 7896402 \"img.html\" >}}"
-	wanted := "https://gist.github.com/spf13/7896402"
-
-	// When
-	result := scanContentForShortcodes(shortcode)
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`scanContentForShortcodes() = %s, want %s`, result, wanted)
-	}
-}
-
-func TestScanContentForShortCodesForSimpleInstagram(t *testing.T) {
-	// Given
-	shortcode := "{{< instagram BWNjjyYFxVx >}}"
-	wanted := "https://www.instagram.com/p/BWNjjyYFxVx"
-
-	// When
-	result := scanContentForShortcodes(shortcode)
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`scanContentForShortcodes() = %s, want %s`, result, wanted)
-	}
-}
-
-func TestScanContentForShortCodesForComplexInstagram(t *testing.T) {
-	// Given
-	shortcode := "{{< instagram BWNjjyYFxVx hidecaption >}}"
-	wanted := "https://www.instagram.com/p/BWNjjyYFxVx"
-
-	// When
-	result := scanContentForShortcodes(shortcode)
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`scanContentForShortcodes() = %s, want %s`, result, wanted)
-	}
-}
-
-func TestScanContentForShortCodesForTwitter(t *testing.T) {
-	// Given
-	shortcode := "{{< tweet 877500564405444608 >}}"
-	wanted := "https://twitter.com/twitter/status/877500564405444608"
-
-	// When
-	result := scanContentForShortcodes(shortcode)
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`scanContentForShortcodes() = %s, want %s`, result, wanted)
-	}
-}
-
-func TestScanContentForShortCodesForVimeo(t *testing.T) {
-	// Given
-	shortcode := "{{< vimeo 146022717 >}}"
-	wanted := "https://player.vimeo.com/video/146022717"
-
-	// When
-	result := scanContentForShortcodes(shortcode)
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`scanContentForShortcodes() = %s, want %s`, result, wanted)
-	}
-}
-
-func TestScanContentForShortCodesForSimpleYoutube(t *testing.T) {
-	// Given
-	shortcode := "{{< youtube w7Ft2ymGmfc >}}"
-	wanted := "https://www.youtube.com/watch?v=w7Ft2ymGmfc"
-
-	// When
-	result := scanContentForShortcodes(shortcode)
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`scanContentForShortcodes() = %s, want %s`, result, wanted)
-	}
-}
-
-func TestScanContentForShortCodesForComplexYoutube(t *testing.T) {
-	// Given
-	shortcode := "{{< youtube id=\"w7Ft2ymGmfc\" autoplay=\"true\" >}}"
-	wanted := "https://www.youtube.com/watch?v=w7Ft2ymGmfc"
-
-	// When
-	result := scanContentForShortcodes(shortcode)
-
-	// Then
-	if result != wanted {
-		t.Fatalf(`scanContentForShortcodes() = %s, want %s`, result, wanted)
+	for name, test := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result := scanContentForShortcodes(test.given)
+			assert.Equal(t, test.expected, result)
+		})
 	}
 }
 
 func TestParsePostWithValidContent(t *testing.T) {
-	// Given
 	postFilePath := "testdata/sample-post.md"
 	language := "en"
 	baseUrl := "https://example.com"
@@ -128,20 +41,18 @@ func TestParsePostWithValidContent(t *testing.T) {
 	var updated time.Time
 	title := "Sample Post"
 	content := "\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae tellus tempus, convallis sapien vitae, ullamcorper odio.\n\nhttps://gist.github.com/spf13/7896402\n\nAliquam vulputate cursus dictum. Integer semper ipsum et ligula euismod, vel dignissim ante consectetur. Donec vel auctor nisi.\n\n#firstCategory #categoryTwo #options #moreOptions"
-	
+
 	wanted := PostToMigrate{
-		body: content,
-		title: title,
-		lang: &language,
-		rtl: &rtl,
+		body:    content,
+		title:   title,
+		lang:    &language,
+		rtl:     &rtl,
 		created: &created,
 		updated: &updated,
 	}
 
-	// When
 	result, _ := parsePost(postFilePath, language, baseUrl, scanForImages)
 
-	// Then
 	if result == wanted {
 		t.Fatalf(`parsePost() failed to parse content correctly`)
 	}
